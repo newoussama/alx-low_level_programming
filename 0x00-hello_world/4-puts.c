b3ccd8b2b3bb5c94d5eb29e7c392a599699086ad
oussama <alouassaaoussama@gmail.com>
2023-06-15 23:43:35 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : a C programe that prints with a put 
+ *
+ * return: always 0 (success)
+*/
+ 
+int main(void)
+{
+        puts("\"programming is like building a multilingual puzzle");
+        return (0);	    
+}    
+
