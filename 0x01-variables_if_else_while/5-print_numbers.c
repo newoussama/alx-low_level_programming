980bbaafbbc258a27f28826836312396569d277e
oussama <alouassaaoussama@gmail.com>
2023-06-17 08:08:40 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: code who print number
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
