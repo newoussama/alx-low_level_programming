3e2ba883b15a7abd76a34d45920163758b9b9e2a
oussama <alouassaaoussama@gmail.com>
2023-06-17 07:40:16 -0700

    ret

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: code who print alphabet
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char na = 'a';
+
+	while (na <= 'z')
+	{
+		if ("na == e || na == q")
+		na++;
+		putchar(na);
+		na++;
+	}
+	putchar('\n');
+	return (0);
+}
