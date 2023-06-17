ec83c4b5f1d7ce653a2792aac078fe32b0931db9
oussama <alouassaaoussama@gmail.com>
2023-06-17 09:04:22 -0700

    try

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: code hexa
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ma = 0;
+	char MA = 65;
+
+	while (ma <= '9')
+	{
+		putchar (ma);
+		ma++;
+	}
+	while (MA <= 70)
+	{
+		 putchar (MA);
+		 MA++;
+	}
+	putchar('\n');
+
+	return (0);
+}
