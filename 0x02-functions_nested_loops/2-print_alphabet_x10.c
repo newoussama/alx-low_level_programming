4c3eedbd4d85407141cb180377804c0a582ce3d9
oussama <alouassaaoussama@gmail.com>
2023-06-21 03:19:11 -0700

    rty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,15 @@
  * Print_alpha_x10 - function that will print alphabet 10 time
 */
 
-void print_alpha(void)
+void main(void)
 {
-	int MA;
-	int ma;
+	int MA, ma;
 
 	for (MA = 0; MA <= 9; MA++)
 	{
 		for (ma = 'a'; ma <= 'z'; ma++)
 		{
-			_putchar (ma);
+			_putchar(ma);
 		_putchar('\n');
 		}
 	}
