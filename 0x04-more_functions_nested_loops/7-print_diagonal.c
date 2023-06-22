1b87cc06fea3e27b871eeac8a1156800c19e47c7
oussama <alouassaaoussama@gmail.com>
2023-06-22 10:16:52 -0700

    tgkl

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonale - print a diagonal whith c language
+ *
+ * Description: a function who print diagonal
+ *
+ * 0n: is the number of time the diagonal be printed
+ *
+ * Return: 0 (success)
+*/
+
+void print_diagonal(int n)
+{
+	if (n > '0' && n <= '9')
+		_putchar(92);
+	else
+		_putchar('\n');
+}
