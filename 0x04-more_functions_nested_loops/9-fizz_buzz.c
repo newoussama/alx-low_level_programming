3c10ec7b63ceda576e8bedc4f3a2e9f06eac0a19
oussama <alouassaaoussama@gmail.com>
2023-06-22 11:05:09 -0700

    AZER

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * description:  a code print numbers from 1 to 100 anf fizz buzz between
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int ma;
+
+	for (ma = 1; ma <= 100; ma++)
+	{
+		if (ma % 3 == 0 && !(ma % 5 == 0))
+		{
+			printf("Fizz");
+		if else(ma % 5 == 0 && !(ma % 3 == 0))
+			printf("Buzz");
+		if else(ma % 3 == 0 && ma % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", ma);
+		if (ma != 100)
+			printf(" ");
+		else
+			printf("\n");
+		}
+		return (0)
+}
