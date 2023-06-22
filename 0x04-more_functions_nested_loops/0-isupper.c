be203b8b506c3983be846bd51ee264f4af5111ba
oussama <alouassaaoussama@gmail.com>
2023-06-22 04:29:10 -0700

    erty

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: code print alphabet upper case
+ *
+ * Return: 1 (success)
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c >= 65;c <= 90; c++)
+		return (1);
+	return (0)
+}
