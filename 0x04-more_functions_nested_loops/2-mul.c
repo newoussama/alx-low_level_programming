773a8fded53049de6ff2fa5c3e3fe36aab604521
oussama <alouassaaoussama@gmail.com>
2023-06-22 07:23:11 -0700

    zsd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiple two integers
+ *
+ * Description: multiplr two integers
+ *
+ * return: 0 (success)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+} 
+
