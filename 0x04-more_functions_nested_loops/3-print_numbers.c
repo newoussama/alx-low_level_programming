9b95731ddbf0b195114532f83bc90de92357f79d
oussama <alouassaaoussama@gmail.com>
2023-06-22 07:34:31 -0700

    scgh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function tha print numbers
+ *
+ * Description: print numbers betwin 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+void print_numbers(void)
+{
+	int ma;
+
+	for (ma >= 48 && ma <= 57)
+		_putchar(ma);
+	putchar('\n');
+}
