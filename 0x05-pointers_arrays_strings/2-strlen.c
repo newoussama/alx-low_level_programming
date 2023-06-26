e0ed140f261b977385c24e6d09c6a833d2c03ca6
oussama <alouassaaoussama@gmail.com>
2023-06-26 11:06:36 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - entry point
+ *
+ * Description: a function that conts the numbers of characters
+ *
+ * return: Always 0 (success)
+*/
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
