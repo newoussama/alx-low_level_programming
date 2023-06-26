531d45a38a4099f069f6764786f3e4f9cd14b476
oussama <alouassaaoussama@gmail.com>
2023-06-26 11:39:02 -0700

    print-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - entry point
+ *
+ * Description: a function that print characters in reverse
+ *
+ * return: Always 0 (success)
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s(i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+
+	}
+	_putchar('\n');
+}
