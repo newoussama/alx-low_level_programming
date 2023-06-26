176dec581f0751dcecfe6f5447efc55fefd2ece0
oussama <alouassaaoussama@gmail.com>
2023-06-26 11:24:45 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - puts a new line in a centense
+ *
+ * Description: a function that print a new line
+ *
+ * return: always 0 (success)
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\0');
+}
