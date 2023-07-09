aba8e335dbfe934f6baf95f3491122eb22bccb00
oussama <alouassaaoussama@gmail.com>
2023-07-09 06:57:10 -0700

    numbere of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: code print the numbers of argument passed on command line
+ *
+ * 0argc: argument that print numbers of argument
+ * 0argv: argument that print names of argument
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
