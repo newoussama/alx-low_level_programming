b59cf66a48121de32049a84a21c8cf110e875d4f
oussama <alouassaaoussama@gmail.com>
2023-07-09 09:24:24 -0700

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: code that print the numbers of coin we need to devise a number
+ *
+ * argc: a argument that cont numbers
+ * argv: a string that contine the name of argument
+ *
+ * return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int b, lcents = 0, mny = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (b = 0; b < 5; b++)
+		{
+			if (mny >= cents[i])
+			{
+				lcents += mny / cents [i];
+				mny = mny % cents [i];
+				if (mny % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", lcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
