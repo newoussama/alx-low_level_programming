9863646714150258ac0a707795a3b92491dfaa1b
oussama <alouassaaoussama@gmail.com>
2023-07-09 06:37:22 -0700

    name file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : code that print his name
+ *
+ * argc: a c language parametre
+ * argv: a c language parametre
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char const *argv[])
+{
+	void(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
