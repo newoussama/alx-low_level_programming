4054217a09315a8ac1f6785d8698267f492dfa58
oussama <alouassaaoussama@gmail.com>
2023-07-09 07:10:07 -0700

    numbere of argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,8 +11,8 @@
  * Return: 0 (success)
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] _attribute_((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
