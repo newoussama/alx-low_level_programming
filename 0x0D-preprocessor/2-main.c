fa9156e87c1fb0fa8da0c11809b7cdde9bf552d4
oussama <alouassaaoussama@gmail.com>
2023-07-17 05:18:55 -0700

    pre

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: a programe that print his name
+ *
+ * @argc: a argument to cont arguments
+ * @argv: contain the arguments
+ *
+ * Return: always 0 (success)
+*/
+
+int main(argc, argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+
+	return (0);
+
+}
