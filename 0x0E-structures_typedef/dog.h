dc738cc4214b9a6a21dd542c24309db2d83506ec
oussama <alouassaaoussama@gmail.com>
2023-07-18 04:43:05 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@
  * of a spacefic dog
 */
 
-struct dog
+typedef struct 
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
