db488f33bcebd0bec096606d967888d680348799
oussama <alouassaaoussama@gmail.com>
2023-07-18 02:45:33 -0700

    dog inf

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _0_DOG_H
+#define _0_DOG_H
+
+/**
+ * struct dog - dog information
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a struct that contain information
+ * of a spacefic dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
