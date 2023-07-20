5de659c729b90034ddceee8fdfdfdc25a762f9e1
oussama <alouassaaoussama@gmail.com>
2023-07-20 05:34:40 -0700

    try

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,5 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * get_op_fu - pointer to a function*/
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * this file contain  5 functions:
+ *
+ * op_add: that add 2 integers
+ * op_sub: that return the difference
+ * op_mul: that return the mul of 2 int
+ * op_div: that devide 2 integers
+ * op_mod: thatgive the remainder
+ *
+ * Description: file to help us in our task
+*/
+
+int op_add(int a, int b)
+	return a + b;
+
+int op_sub(int a, int b)
+	return a - b;
+
+int op_mul(int a, int b)
+	return a * b;
+
+int op_div(int a, int b)
+	return a / b;
+
+int op_mod(int a, int b)
+	return a % b;
