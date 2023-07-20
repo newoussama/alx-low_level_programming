34372b705f1c60be3a695aac30b30a0e61b52328
oussama <alouassaaoussama@gmail.com>
2023-07-20 02:30:03 -0700

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -19,6 +19,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i;
 
 	for (i = 0; i < size; i++)
+	{
 		array[i] = size - 1;
-	action(array[i]);
+		action(array[i]);
+
+	}
 }
