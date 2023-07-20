59c236093d2fccff063f05e618c4e4ad02ef3a6d
oussama <alouassaaoussama@gmail.com>
2023-07-20 04:04:45 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,8 +15,10 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	size = sizeof(array);
-
 	for (int i = 0; i < size; i++)
-		cmp(*array++);
+	{
+		if (cmp(array[i]) != 0)
+			return i;
+		return -1;
+	}
 }
