b58b4df2e0faf907bbd6842d2b49189ce07ade6d
oussama <alouassaaoussama@gmail.com>
2023-07-20 02:07:09 -0700

    iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - a function
+ *
+ * @array: a pointer to an anteger array
+ * @size: the size of the array
+ * @action: a pointer to a function
+ *
+ * Description: a function that call atthor functions
+ *
+ * Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	action(array, size);
+}
