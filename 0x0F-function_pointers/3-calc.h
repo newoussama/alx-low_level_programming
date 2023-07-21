b0b562cd1291cc7a1f6ff16e92ef11e29194704c
oussama <alouassaaoussama@gmail.com>
2023-07-21 03:42:11 -0700

    ret

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,20 @@
 #ifndef _3_CALC_H
 #define _3_CALC_H
 
+typedef struct
+{
+	char *operator;
+	int (*fun)(int, int);
+       }op_t;
+
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
+typedef (*oppe)(int, int);
 int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,33 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
- * get_op_fu - pointer to a function*/
+ * get_op_fu - pointer to a function
+ *
+ * @s: the string implemented by user
+ *
+ * Description: a pointer to function 
+ *
+ * return : integer
+*/
+
+oppe get_op_func(char *s)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; ops[i].operator != NULL; i++)
+	{
+		if (strcmp(s, ops[i].operator) == 0)
+			return (ops.func);
+	}
+	return NULL;	
+}
