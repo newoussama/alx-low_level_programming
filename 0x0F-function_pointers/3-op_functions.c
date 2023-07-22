5b04f7b2e7dcc1fe8d3ba4dc517a2ed8fb625786
oussama <alouassaaoussama@gmail.com>
2023-07-22 07:08:23 -0700

    oi

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -14,16 +14,26 @@
 */
 
 int op_add(int a, int b)
+{
 	return a + b;
+}
 
 int op_sub(int a, int b)
+{
 	return a - b;
+}
 
 int op_mul(int a, int b)
+{
 	return a * b;
+}
 
 int op_div(int a, int b)
+{
 	return a / b;
+}
 
 int op_mod(int a, int b)
+{
 	return a % b;
+}
