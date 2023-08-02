cff639a0db45aa0ae955b37c17eaaea38b4112b4
newoussama <alouassaaoussama@gmail.com>
2023-08-02 01:46:51 -0700

    rec put

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,10 +13,8 @@
 
 void _puts_recursion(char *s)
 {
-	int n = 0;
-
-	if (s[n] == '\0')
+	if (*s == '\0')
 		return;
-	_putchar(s[n]);
-	_puts_recursion(s[n] + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
