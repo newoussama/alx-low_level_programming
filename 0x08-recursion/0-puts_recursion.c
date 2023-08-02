e71e139754082d0b872b4ae13c9da2856e22a272
newoussama <alouassaaoussama@gmail.com>
2023-08-02 01:36:55 -0700

    rec put

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion: recursive function
+ *
+ * @s: a pointer for a string
+ *
+ * Description: a function that put a string
+ *
+ * return: (s) success
+*/
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+		return;
+	_putchar(s);
+	_puts_recursion(s + 1);
+}
