ae826911ad0409378477f28ca6a0deb23993bfb8
newoussama <alouassaaoussama@gmail.com>
2023-08-02 04:04:09 -0700

    square_recur

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,22 +8,26 @@
  *
  * Description: a function that give a natural square
  *		rout of a number
- * 
+ *
  * Return: result (success)
 */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	if (n == 0)
 	{
-		return (-1);
+		return (0);
 	}
 	else if (n == 1)
 	{
 		return (1);
 	}
-	else
+	else if (n > 1)
 	{
 		return (n + _sqrt_recursion(n - 1) + n - 1);
+	}
+	else
+	{
+		return (-1);
 	}
 }
