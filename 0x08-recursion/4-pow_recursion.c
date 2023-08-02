ec3ab17baeecc331691953e1f542ef0badfaf2d7
newoussama <alouassaaoussama@gmail.com>
2023-08-02 03:09:26 -0700

    pow_recur

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion: recursive function
+ *
+ * @x: integer
+ * @y: integer
+ *
+ * Description: a function that powered a number and another number
+ *
+ * Return: the result (success)
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (y * _pow_recursion(x, x));
+	}
+}
