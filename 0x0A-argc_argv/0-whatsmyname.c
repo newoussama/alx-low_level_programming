db427c0a1621d24911df6ed0cd51fa65e16a9f01
newoussama <alouassaaoussama@gmail.com>
2023-08-04 03:58:05 -0700

    whatsname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: size of commend line arguments
+ * @argv: array that contain the arguments
+ *
+ * Description: a function that print the name of a program
+ *
+ * Return : 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
