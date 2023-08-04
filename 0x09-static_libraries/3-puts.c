7668888f1c72af9b4cfdde7ae9ed925f9ad3ba82
newoussama <alouassaaoussama@gmail.com>
2023-08-04 03:18:48 -0700

    libmy

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
-void _puts(char *s);
+/**
+ * _puts - puts a new line in a centense
+ *
+ * Description: a function that print a new line
+ *
+ * return: always 0 (success)
+*/
+
+void _puts(char *str)
+{
+        while (*str != '\0')
+        {
+                _putchar(*str + 0);
+                str++;
+        }
+        _putchar('\n');
+}
