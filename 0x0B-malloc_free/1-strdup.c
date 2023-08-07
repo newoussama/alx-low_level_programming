3e7cf36e07369850ba02b19818ba48de79c40930
newoussama <alouassaaoussama@gmail.com>
2023-08-07 04:14:24 -0700

    strup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup: function that print a string
+ *
+ * @str: a string that we can copy on our
+ *	memory allocated
+ *
+ * Description: function that allocate memory and pass on it a
+ *	string
+ *
+ * Return: NULL OR POINTER (success)
+*/
+
+char *_strdup(char *str)
+{
+	char *pvr;
+	int lt;
+
+	lt = strlen(str);
+
+	pvr = (char *)malloc(lt + 1) * (sizeof(char));
+
+	for (i = 0; i <= lt; i++)
+	{
+		pvr[i] = str[i];
+	}
+	if (str == NULL || pvr == NULL)
+		return (NULL);
+	pvr[lt] = '\0';
+
+	return (pvr);
+}
