24f8904b556001c85f92fafab30b61d45f14940d
newoussama <alouassaaoussama@gmail.com>
2023-08-08 00:48:11 -0700

    2d

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,12 +26,7 @@ int **alloc_grid(int width, int height)
 		arr2D[i] = (int *)malloc(width * (sizeof(int)));
 
 		if (arr2D[i] == NULL) 
-		{
-			for (j = 0; j < i; j++)
-                		free(arr2D[j]);
-		}
-		free(arr2D);
-		return (NULL);
+			return (NULL);
 	}
 	for (j = 0; j < width; j++)
 			arr2D[i][j] = 0;
