9a7fd5862b5e23b23c152c61b1e1bfc705497539
newoussama <alouassaaoussama@gmail.com>
2023-08-10 10:10:55 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,6 +4,24 @@
 #include <string.h>
 
 /**
+ * _memset: a function that copy a string in another memory loca
+ * 
+ * @m: pointer to put the constant
+ * @f:canstant
+ * @l: max size to use
+ *
+ * return: pointer
+*/
+char *_memset(char *m; char f; unsigned int l)
+{
+	char *pvr = m;
+
+	while (m--)
+		*m++ = f;
+	return (pvr);
+}
+
+/**
  * _calloc: memory allocated initialised to zero
  *
  * @nmemb: number of element
@@ -15,22 +33,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *pgr;
-	unsigned int i;
-	unsigned char *bytePgr;
 
 	pgr = malloc(nmemb * size);
-
-	bytePgr = (unsigned char *)pgr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	pgr = malloc(nmemb * size);
 
 	if (pgr == NULL)
 		return(NULL);
 
-	for (i = 0; i < nmemb * size; i++)
-		bytePgr[i] = 0;
+	_memset(pgr, 0, nmemb * size);
 
 	return (pgr);
 }
