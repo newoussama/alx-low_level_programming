cb62ee0bd98820a412fd8115e8dd42472d46f9bd
newoussama <alouassaaoussama@gmail.com>
2023-08-11 03:29:03 -0700

    ret

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	void *m;
 
+	if (new_size == old_size)
+		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
