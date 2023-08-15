803ec4b0d813e4da8282db936d22edcbe2259f34
newoussama <alouassaaoussama@gmail.com>
2023-08-15 03:43:53 -0700

    NAME

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a programme that print the name of a file
+ *
+ * return: return 0 in the value of the programme (success)
+*/
+
+int main(void)
+{
+	printf("%s", __FILE__);
+}
