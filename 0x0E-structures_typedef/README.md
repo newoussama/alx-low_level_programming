a9c3b60e6047ca7198c18618f0d3958a790142ca
newoussama <alouassaaoussama@gmail.com>
2023-08-15 04:29:33 -0700

    dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this file is readme file for this task
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,10 @@
+#ifndef DOG_H
+#define DOG_H
+
+struct dog {
+	char *name; /* the name of the dog*/
+	float age; /* the age of the dog*/
+	char *owner; /* the owner of the dog*/
+};
+
+#endif
