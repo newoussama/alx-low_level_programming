1ecceb1f4b929f53560b359d313ed82137918735
newoussama <alouassaaoussama@gmail.com>
2023-09-21 07:58:34 -0700

    link list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,20 +13,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *nw;
+	list_t *new;
 	unsigned int ln = 0;
 
 	while (str[ln])
 		ln++;
 
-	nw = malloc(sizeof(list_t));
-	if (!nw)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	nw->str = strdup(str);
-	nw->ln = ln;
-	nw->next = (*head);
-	(*head) = nw;
+	new->str = strdup(str);
+	new->ln = ln;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
